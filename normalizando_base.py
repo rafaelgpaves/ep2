b78c6ea435c71353d0b1c09acf115f3424082c26
koeitsu <brun0zalc@hotmail.com>
2022-04-25 10:28:12 -0300

    juntando todas as funções em um único arquivo

--- normalizando_base.py
+++
@@ -1,9 +0,0 @@
-## {continente: {país: {info: "num"}}}
-
-def normaliza(continentes):
-    normalizado = {}
-    for continente in continentes:
-        for pais in continentes[continente]:
-            normalizado[pais] = continentes[continente][pais]
-            normalizado[pais]["continente"] = continente
-    return normalizado
--- sorteia_pais.py
+++
@@ -1,5 +0,0 @@
-from random import choice
-
-def sorteia_pais(paises):
-    sorteado = choice(list(paises))
-    return sorteado
