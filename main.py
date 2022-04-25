80c673950dfbc0d8322a9eb27284ac542244a3a7
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2022-04-25 10:32:10 -0300

    mais condicoes iniciais

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+"""importando funcoes:"""
+from funcoes import *
+
 """condicoes iniciais do jogo:"""
 tentativas = 20 #variavel que guarda o numero de tentativas restantes do jogador
-
+pais_sorteado = sorteia_pais()
 
 print("Um país foi escolhido, tente adivinhar!")
 
@@ -9,4 +12,9 @@ jogo = True
 while jogo:
 
     print("Você tem {} tentativas".format(tentativas))
+
     entrada = input("Qual o seu palpite? ")
+
+    if entrada == "desisto":
+
+        jogo = False
